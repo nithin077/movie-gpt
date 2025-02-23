2f2ab0e6436268aad27c2344a9925b4b3f3d4651
nithin077 <nithinchinn@gmail.com>
2025-02-23 23:31:27 +0530

    MovieDeatils route added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router'
 import useMovieDetailsById from '../hooks/useMovieDetailsById';
 import { useSelector } from 'react-redux';
+import { IMG_CDN } from '../sharedcomponents/Constant';
 
 const MovieDetails = () => {
 
@@ -12,12 +13,13 @@ const MovieDetails = () => {
     console.log(movieDetails)
 
   return (
-    <div className='pt-[45%] md:pt-[10%] flex flex-row pl-10'>
-        <div className=''>
-            Image
+    <div className='pt-[45%] md:pt-[10%] flex flex-row pl-10 '>
+        <div className='w-80'>
+            <img src={IMG_CDN + movieDetails?.poster_path} alt="MovieImage" className='rounded-lg'/>
         </div>
-        <div className=''>
-            Details
+        <div className='p-4'>
+          <p className='text-4xl'>{movieDetails?.original_title}</p>
+          <p className='pt-4'>{movieDetails?.release_date}</p>
         </div>
     </div>
   )
