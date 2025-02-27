a28d1106340a4d811743c4ffaa61833a4b5f05b9
nithin077 <nithinchinn@gmail.com>
2025-02-28 00:48:07 +0530

    Updated the watch List

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -4,9 +4,11 @@ import { useSelector } from 'react-redux';
 
 const WatchList = () => {
   const movieDetails = useSelector(store => store.movies.watchListMovies);
-  const isWatchListed = true
+  const isWatchListed = true;
+
+  //Adding the Watch List UI
   return (
-    <div className='pt-[10%] mx-14 '>
+    <div className='pt-[10%] mx-14'>
       <div className='bg-slate-500 p-10 h-screen mx-14 w-11/12 flex flex-row'>
           {movieDetails.map(movie => (
             <MovieCards key={movie?.id} movie={movie} isWatchListed={isWatchListed}/>
